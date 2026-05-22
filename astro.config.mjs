21080f1253be5e3b007bfe58f3f8ef7b4c37d5c9
Nicolas Garcia <playchathac@gmail.com>
2026-05-22 11:36:52 +0200

    tailwind...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+import svelte from '@astrojs/svelte';
+
+// @ts-ignore: optional dev dependency may not have type declarations in this environment
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte({extensions: ['.svelte']}), tailwind()],
+});
